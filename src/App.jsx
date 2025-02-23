1273088f4a9a89abc484dd925cda8a2a4b6cbf07
IvanovKS <Skitchi@mail.ru>
2025-02-23 22:13:32 +0300

    Add some dependencies, add first empty components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import Settings from './pages/Settings';
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <div className="app">
+      <Home />
+      <NotFound />
+      <Settings />
+    </div>
+  );
 }
 
 export default App;
