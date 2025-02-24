5669b2a070e6ba3ca70e83215542eda8340ceda7
IvanovKS <Skitchi@mail.ru>
2025-02-24 13:12:01 +0300

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 import Settings from './pages/Settings';
+import About from './pages/About';
+import MainLayout from './layouts/MainLayout';
 
 function App() {
   return (
-    <div className="app">
-      <Home />
-      <NotFound />
-      <Settings />
-    </div>
+    <BrowserRouter>
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';
 
 function MainLayout() {
-  return <div>MainLayout</div>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default MainLayout;
